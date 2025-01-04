ce81737348a88719ddce24ada2b465ee954addbc
arudim <adalbertmid@gmail.com>
2025-01-04 16:37:21 +0100

    odzyskanie

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/deploy_Baza_Danych_bf4200f23b.cmake
@@ -0,0 +1,8 @@
+include(C:/Users/Wojtek/Documents/Baza_Danych/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/Baza_Danych-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE C:/Users/Wojtek/Documents/Baza_Danych/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Baza_Danych.exe
+    GENERATE_QT_CONF
+)

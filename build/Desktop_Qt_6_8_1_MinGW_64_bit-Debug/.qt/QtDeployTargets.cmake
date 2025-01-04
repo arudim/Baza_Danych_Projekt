5f2c4c08daa8002b0c22d1cf7aaeb6455ffa1b35
arudim <adalbertmid@gmail.com>
2025-01-04 16:31:14 +0100

    gitignore

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Baza_Danych_FILE C:/Users/Wojtek/Documents/Baza_Danych/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Baza_Danych.exe)
-set(__QT_DEPLOY_TARGET_Baza_Danych_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Baza_Danych_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
